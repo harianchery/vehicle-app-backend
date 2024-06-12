3c5627258988f88540c8760860105f1b4f1963a5
harianchery <hari.anchery001@gmail.com>
2024-06-12 18:28:09 +0530

    connected with db

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const mongoose=require("mongoose")
 const cors=require("cors")
 const express=require("express")
+const { vehiclemodel } = require("./models/vehicle")
 
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 
-
+mongoose.connect("mongodb+srv://hari:hari001@cluster0.ocavfn3.mongodb.net/vehicledb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/add",(req,res)=>{
     let input=req.body
-    console.log(input)
+    // console.log(input)
+    let vehicle=new vehiclemodel(input)
+    vehicle.save()
     res.json({"status":"success"})
 })
 
